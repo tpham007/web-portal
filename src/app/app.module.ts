e9c2289975f5a636775126c78582265b4dab3b83
tpham007 <tpcalif@yahoo.com>
2018-10-20 15:03:05 -0700

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LeftPanelComponent,
+    RightPanelComponent,
+    FooterComponent,
+    HeaderComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
