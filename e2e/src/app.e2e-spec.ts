53e44ba4a4dc20d99cf2fac6bbc7a90884a07f43
trung pham <trung.pham@harmonicinc.com>
2018-10-26 10:56:54 -0700

    test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display app logo', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to web-portal!');
+    expect(page.getLogo()).toContain('logoA.png');
   });
 });
