16a0dad1b25ad84146d3d518b3bc5fb6c5fdbaee
trung pham <trung.pham@harmonicinc.com>
2018-10-26 10:52:12 -0700

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('web-portal');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render app log', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to web-portal!');
+    expect(compiled.querySelector('#appLogo').getAttribute('src')).toContain('logoA.png');
   });
 });
