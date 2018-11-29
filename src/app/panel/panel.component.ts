2448e391044dd6f216da2f212d3715d2cef16f6a
tpham007 <tpcalif@yahoo.com>
2018-11-29 12:53:40 -0800

    mobile app panel

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -24,6 +24,7 @@ export class PanelComponent implements OnInit {
               setTimeout( () => {
                 THIS.repo.shareCollection.selectedApp = app;
                 THIS.repo.shareCollection.shownAppType = THIS.repo.shareCollection.selectedApp.type;
+                THIS.shareCollection.showApp = false;
               }, 300);
             }
           });
