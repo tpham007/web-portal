f515fe49037fb6982641a969c768aaa11272c396
tpham <trung.pham@harmonicinc.com>
2019-03-18 11:51:39 -0700

    active link

--- projects/customize/src/lib/tabset/tabset.component.ts
+++ projects/customize/src/lib/tabset/tabset.component.ts
@@ -7,27 +7,13 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './tabset.component.html',
   styleUrls: ['./tabset.component.css']
 })
-export class TabsetComponent implements OnInit, AfterContentInit  {
+export class TabsetComponent implements OnInit  {
   @Input() vertical;
   @Output() onSelect = new EventEmitter();
   @ContentChildren(TabComponent) tabs;
-  activeTab: any;
-  constructor(private route: ActivatedRoute) { }
+  
+  constructor() { }
 
   ngOnInit() {
-    this.activeTab = this.route.snapshot['_routerState'].url.split('/', 3)[2];
-  }
-  ngAfterContentInit() {
-    const tabs = this.tabs.toArray();
-    const actives = this.tabs.filter(t => { return t.active });
-
-    if(actives.length > 1) {
-      console.error(`Multiple active tabs set 'active'`);
-    } else if(!actives.length && tabs.length) {
-      tabs[0].active = true;
-    }
-  }
-  public isActive(tab) {
-    return tab == this.activeTab;
   }
 }
